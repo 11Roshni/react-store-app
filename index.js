3057416102f22a32977f7c5bc78893e13dcb8788
11Roshni <98761096+11Roshni@users.noreply.github.com>
2022-10-01 21:55:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { connectDB } = require("./utils/db");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
+
+
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(process.env.PORT || 5001, () => {
+    console.log("backend is running!!");
+  });
+};
+
+startServer();
